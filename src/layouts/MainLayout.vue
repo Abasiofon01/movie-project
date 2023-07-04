a6bfae39796c932e74a97053909ed43b76d76ee7
abasiofon01 <abasiofonudonkang@gmail.com>
2023-07-04 18:07:51 +0100

    style: minor stylings

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header class="sticky">
+    <q-header>
       <q-toolbar>
         <div class="mobile-menu-btn">
           <q-btn
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,6 @@
 <template>
   <q-page>
+    <PopularMovies />
     <HeroPage />
     <UpcomingMovies />
     <ServicesSection />
@@ -7,6 +8,18 @@
     <TopSeries />
     <CTA />
     <FooterSection />
+
+    <q-page-scroller
+      position="bottom-right"
+      :scroll-offset="150"
+      :offset="[18, 18]"
+    >
+      <q-btn
+        fab
+        icon="keyboard_arrow_up"
+        style="background-color: hsl(57, 97%, 45%); color: white"
+      />
+    </q-page-scroller>
   </q-page>
 </template>
 
@@ -14,6 +27,7 @@
 import { defineComponent } from "vue";
 import CTA from "src/components/CTA.vue";
 import HeroPage from "src/components/HeroPage.vue";
+import PopularMovies from "src/components/PopularMovies.vue";
 import TopRated from "src/components/TopRated.vue";
 import TopSeries from "src/components/TopSeries.vue";
 import FooterSection from "src/components/FooterSection.vue";
@@ -29,6 +43,7 @@ export default defineComponent({
     TopRated,
     TopSeries,
     CTA,
+    PopularMovies,
     FooterSection,
   },
 });
